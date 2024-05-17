273948b3723d60b92929030a3e535a108b26d36a
TestowyTest <pawelvxq@op.pl>
2024-05-17 16:15:30 +0200

    dodanie reszty zadan

--- Lab4/scripts.js
+++ Lab4/scripts.js
@@ -37,28 +37,70 @@ function pelnoletni() {
 }
 
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.changeColor');
+
+function resetButtons() {
+    buttons.forEach(button => {
+      button.classList.remove('clicked-bg');
+      button.classList.add('default-bg');
+    });
+  }
 
 buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    button.style.backgroundColor = getRandomColor();
-    console.log("test")
-  });
+button.addEventListener('click', function() {
+
+    resetButtons();
+
+    button.classList.remove('default-bg');
+    button.classList.add('clicked-bg');
+});
 });
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+
+const toggleButton = document.getElementById('displayChange');
+const myElement = document.getElementById('textbox');
+
+toggleButton.addEventListener('click', () => {
+    if (myElement.style.display === 'none') {
+        myElement.style.display = 'block';
+    } else {
+        myElement.style.display = 'none';
+    }
+});
+
+function addToList() {
+    var input = document.getElementById("textInput").value;
+    var list = document.getElementById("list");
+    var listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(input));
+    list.appendChild(listItem);
+    document.getElementById("textInput").value = "";
 }
 
+function addToTable() {
+    var name = document.getElementById("nameInput").value;
+    var surname = document.getElementById("surnameInput").value;
+    var table = document.getElementById("tableBody");
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = name;
+    cell2.innerHTML = surname;
+    document.getElementById("nameInput").value = "";
+    document.getElementById("surnameInput").value = "";
+}
 
+function convertToFar() {
+    var celsius = document.getElementById("celsiusInput").value;
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("result1").innerHTML = "Wynik: " + fahrenheit.toFixed(2) + " stopni Fahrenheita";
+}
 
-
-
+function convertToCel() {
+    var fahrenheit = document.getElementById("fahrenheitInput").value;
+    var celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("result2").innerHTML = "Wynik: " + celsius.toFixed(2) + " stopni Celsiusza";
+}
 
 pelnoletni()
 odliczanie()
